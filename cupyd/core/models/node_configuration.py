08b58244732fcee430c1df0188fc0e775d40915f
jalorub <fjalorub@gmail.com>
2025-02-18 17:57:35 +0100

    0.3.0 changes
    - Limit Connector queues
    - Allow setting max queue sizes
    - Allow setting custom progress refresh interval
    - Add documentation & changelog URLs to pyproject.toml

--- cupyd/core/models/node_configuration.py
+++ cupyd/core/models/node_configuration.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional
+
+DEFAULT_QUEUE_MAX_SIZE = 10_000
 
 
 @dataclass
@@ -12,6 +14,7 @@ class ExtractorConfiguration:
 class TransformerConfiguration:
     input_key: str = None
     run_in_main_process: bool = False
+    queue_max_size: Optional[int] = DEFAULT_QUEUE_MAX_SIZE
 
 
 @dataclass
@@ -20,6 +23,7 @@ class FilterConfiguration:
     value_to_filter: Any = None
     disable_safe_copy: bool = False
     run_in_main_process: bool = False
+    queue_max_size: Optional[int] = DEFAULT_QUEUE_MAX_SIZE
 
 
 @dataclass
@@ -27,12 +31,15 @@ class LoaderConfiguration:
     input_key: str = None
     disable_safe_copy: bool = False
     run_in_main_process: bool = False
+    queue_max_size: Optional[int] = DEFAULT_QUEUE_MAX_SIZE
 
 
 @dataclass
 class BulkerConfiguration:
     run_in_main_process: bool = False
+    queue_max_size: Optional[int] = DEFAULT_QUEUE_MAX_SIZE
 
 
 class DeBulkerConfiguration:
     run_in_main_process: bool = False
+    queue_max_size: Optional[int] = DEFAULT_QUEUE_MAX_SIZE
